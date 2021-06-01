46950238207c18d0b806dd3258c14744f9ec7bdb
Allan Foppa <allanfoppa@hotmail.com>
2021-06-01 13:00:45 -0300

    Change query and populate on webservice and masonry to grid

--- webservice/src/repositories/recipes.repository.js
+++ webservice/src/repositories/recipes.repository.js
@@ -1,7 +1,7 @@
 import { query } from "../config/database/index.js";
 
 async function getRecipes() {
-    const { rows } = await query('SELECT * FROM recipes');
+    const { rows } = await query('SELECT id, name, image, thumbs_up, thumbs_down FROM recipes');
     return rows
 }
 

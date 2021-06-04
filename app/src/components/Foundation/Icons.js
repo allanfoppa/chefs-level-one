2d1106f7d5bf8ef1fb926f22b62501078a735e91
Allan Foppa <allanfoppa@hotmail.com>
2021-06-04 16:05:00 -0300

    Change color thumbs

--- app/src/components/Foundation/Icons.js
+++ app/src/components/Foundation/Icons.js
@@ -3,13 +3,13 @@ import { FaRegThumbsUp, FaRegThumbsDown } from 'react-icons/fa'
 
 export const ThumbsUp = () => {
     return(
-        <FaRegThumbsUp />
+        <FaRegThumbsUp color="#4867aa" />
     )
 }
 
 export const ThumbsDown = () => {
     return(
-        <FaRegThumbsDown />
+        <FaRegThumbsDown color="#4867aa" />
     )
 }
 
